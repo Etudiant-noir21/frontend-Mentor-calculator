768fa1580679c6d83745b89dc05ae6714aaf2742
Baba Faye <fayebaba81@gmail.com>
2024-11-20 01:10:16 +0000

    changement disposition

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// importation des fonctions
+import {calculete} from '/assets/js/functions/function.js';
+calculete();
+
+// importation du theme
+import {theme} from '/assets/js/functions/theme.js';
+theme();
--- calculator-app-main/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-// importation des fonctions
-import {calculete} from '/calculator-app-main/assets/js/functions/function.js';
-calculete();
-
-// importation du theme
-import {theme} from '/calculator-app-main/assets/js/functions/theme.js';
-theme();
